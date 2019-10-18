5ec116bf52e7182135f5101efddf647c7bd0bdec
Edward <edwardhall@csus.edu>
2019-10-18 15:10:56 -0700

    correction to meeting.js
    corrected meeting.js

--- commands/meeting_time.js
+++ commands/meeting_time.js
@@ -3,9 +3,7 @@ module.exports = {
 	description: 'Returns the day, hour and location of general meetings.',
 	cooldown: 5,
 	execute(message, args) {
-		if (!args.length) {
-			return message.channel.send('You did not provide any arguments!');
-		} else if (args[0] === 'meeting') {
+		if (args[0] === 'meeting') {
 			return message.channel.reply(
 				'${message.author} Meeting times are every Friday at 3pm in Mendocino Hall 1032.'
 			);
